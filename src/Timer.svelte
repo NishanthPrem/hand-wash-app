f8c4a53eddc274e34a4ee17cb3cb44b98b64cb57
Nishanth <nishanthprem8@gmail.com>
2022-03-05 12:38:51 +0530

    Updated.

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,53 +1,53 @@
 <script>
-import ProgressBar from "./ProgressBar.svelte";
-    const totalSeconds = 5
-    let secondsLeft = totalSeconds
-    let isRunning = false
-    $: progress = (totalSeconds - secondsLeft)/totalSeconds * 100
-
-    function startTimer(){
-        isRunning = true
-        const timer = setInterval(() => {
-        secondsLeft -=1
-        if (secondsLeft ==0) {
-            clearInterval(timer)
-            isRunning = false
-            secondsLeft = totalSeconds
+    import { createEventDispatcher } from "svelte";
+    import ProgressBar from "./ProgressBar.svelte";
+    const totalSeconds = 20;
+    let secondLeft = totalSeconds;
+    let isRunning = false;
+    $: progress = ((totalSeconds - secondLeft) / totalSeconds) * 100;
+    const dispatch = createEventDispatcher();
+    function startTimer() {
+      isRunning = true;
+      const timer = setInterval(() => {
+        secondLeft -= 1;
+        if (secondLeft == 0) {
+          clearInterval(timer);
+          isRunning = false;
+          secondLeft = totalSeconds;
+          dispatch("end");
         }
-    }, 1000);
+      }, 1000);
     }
-</script>
-
-<style>
-    h2{
-        margin: 0%;
+  </script>
+  
+  <style>
+    h2 {
+      margin: 0;
     }
-    .start{
-        background-color: rgb(250, 93, 93);
-        width: 100%;
-        padding: 10px 0;
+    .start {
+      background-color: rgb(154, 73, 73);
+      width: 100%;
+      margin: 10px 0;
     }
-
-    .start[disabled]{
-        border: solid rgb(29, 23, 90) 1px;
-        cursor: not-allowed;
+    .start[disabled] {
+      background-color: rgb(194, 194, 194);
+      cursor: not-allowed;
     }
-
-</style>
-
-<div bp ='grid'>
-    <h2 bp= "offset-5@md 4@md 12@sm">
-        Seconds Left: {secondsLeft}
-    </h2>
-</div>
-
-<ProgressBar {progress} />
-
-<div bp ='grid'>
-    <button 
-    on:click = {startTimer} 
-    disabled = {isRunning} 
-    class="start" bp= "offset-5@md 4@md 12@sm">
-    <span style="color: black"> START</span> 
+  </style>
+  
+  <div bp="grid">
+    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: {secondLeft}</h2>
+  </div>
+  <ProgressBar {progress} />
+  
+  <div bp="grid">
+  
+    <button
+      disabled={isRunning}
+      on:click={startTimer}
+      bp="offset-5@md 4@md 12@sm"
+      class="start">
+      Start
     </button>
-</div>
+  
+  </div>
